286a0daa2a28930d43836b71e9d4a8006bb3c8b6
Morten Muusmann <morten.muusmann@gmail.com>
2025-12-14 23:57:05 +0100

    Fix: Tilføj landing page til forsiden

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+// app/page.tsx
 import LigusterLandingPage from "../components/LigusterLandingPage";
 
-export default function Page() {
-  return <LigusterLandingPage />;
+export default function Home() {
+  return (
+    <main>
+      <LigusterLandingPage />
+    </main>
+  );
 }
